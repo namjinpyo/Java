eccebdf637c0042afe1b83a59a2d48b06985af14
namjp <namjinpyo@naver.com>
2020-09-15 23:08:16 +0900

    Initial Create

---
+++ Java/HelloWorldWithVisualStudioCode/HelloWorldWithVisualStdioCode.java
@@ -0,0 +1,9 @@
+/**
+ * HelloWorldWithVisualStdioCode
+ */
+public class HelloWorldWithVisualStdioCode {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
